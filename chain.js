31ebb9965b6f82229051f1bc861380f288931a62
바디 언니 <79979508+vaadi12@users.noreply.github.com>
2021-04-22 18:52:17 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,19 @@
+class Chain {
+    constructor(body1,body2){
+        var options={
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:0.05,
+            length:6
+
+        }
+       this.chain=Constraint.create(options);
+       World.add(world,this.chain);
+    }
+display(){
+    var pointA=this.chain.bodyA.position;
+    var pointB=this.chain.bodyB.position;
+    strokeWeight(3);
+   line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+}
